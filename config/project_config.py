6d50008339c1120952eb9f8cd35d23e7d6ac27f2
svurity1 <svurity@hawk.iit.edu>
2026-03-28 12:18:05 -0500

    project setup: repo structure, config, dependencies

---
+++ config/project_config.py
@@ -0,0 +1,12 @@
+TOTAL_ELIGIBLE_USERS = 50000
+SAMPLE_SIZE_PER_GROUP = 4000
+ALPHA = 0.05
+POWER = 0.80
+RANDOM_SEED = 42
+
+TESTS = {
+    1: "Targeting / Personalization",
+    2: "Offer Design",
+    3: "Channel & Timing",
+    4: "Application UX",
+}
